e12ce7ce0435b375ca0591d2f3add972fd2afb38
Low Jung Xuan <lowjungxuan@gmail.com>
2022-10-30 10:23:33 +0800

    ticker

--- Sources/StocksAPI/Models/Ticker.swift
+++ Sources/StocksAPI/Models/Ticker.swift
@@ -1,53 +1,47 @@
-//
-//  File.swift
-//
-//
-//  Created by MD1 on 22/10/2022.
+//  Created by Alfian Losari on 13/08/22.
 //
 
 import Foundation
+
 public struct SearchTickersResponse: Decodable {
-    public let data: [Ticker]?
+    
     public let error: ErrorResponse?
+    public let data: [Ticker]?
+    
     enum CodingKeys: CodingKey {
+        case count
         case quotes
         case finance
     }
-
+    
     enum FinanceKeys: CodingKey {
         case error
     }
-
+    
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         data = try? container.decodeIfPresent([Ticker].self, forKey: .quotes)
-        error = try? container.nestedContainer(keyedBy: FinanceKeys.self, forKey: .finance).decodeIfPresent(ErrorResponse.self, forKey: .error)
+        error = try? container.nestedContainer(keyedBy: FinanceKeys.self, forKey: .finance)
+            .decodeIfPresent(ErrorResponse.self, forKey: .error)
     }
 }
 
 public struct Ticker: Codable, Identifiable, Hashable, Equatable {
     public let id = UUID()
+    
     public let symbol: String
     public let quoteType: String?
     public let shortname: String?
-    public let longName: String?
+    public let longname: String?
     public let sector: String?
     public let industry: String?
     public let exchDisp: String?
-
-    public init(
-        symbol: String,
-        quoteType: String? = nil,
-        shortname: String? = nil,
-        longName: String? = nil,
-        sector: String? = nil,
-        industry: String? = nil,
-        exchDisp: String? = nil
-    ) {
+    
+    public init(symbol: String, quoteType: String? = nil, shortname: String? = nil, longname: String? = nil, sector: String? = nil, industry: String? = nil, exchDisp: String? = nil) {
         self.symbol = symbol
         self.quoteType = quoteType
         self.shortname = shortname
-        self.longName = longName
+        self.longname = longname
         self.sector = sector
         self.industry = industry
         self.exchDisp = exchDisp
